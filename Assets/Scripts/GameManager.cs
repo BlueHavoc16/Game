3aa461a55147d75e04694b166b3647bac47299ae
BlueHavoc16 <Jaidinw@icloud.com>
2024-04-26 17:37:49 -0400

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int scorePlayer1, scorePlayer2;
+
+    public void OnScoreZoneReached(int id)
+    {
+
+    }
+}
